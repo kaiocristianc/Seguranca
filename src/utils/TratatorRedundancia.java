acd84a39236628cb1769f875de202f11a9b5b445
Kaio Cristian Costa <kaio.zeroglosa@gmail.com>
2013-12-17 21:35:33 -0300

    Requisição travada, não muito bem eu diria.

---
+++ src/utils/TratatorRedundancia.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kaio
+ * Date: 12/17/13
+ * Time: 9:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TratatorRedundancia {
+}
