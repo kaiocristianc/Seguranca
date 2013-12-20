522d96c0cf0e0d2c6d8c80fea2e3db0f202f42e5
Kaio Cristian Costa <kaio.zeroglosa@gmail.com>
2013-12-20 07:14:00 -0300

    finalizando

---
+++ src/Test/ArquivoUtilsTeste.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kaio
+ * Date: 12/18/13
+ * Time: 11:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArquivoUtilsTeste {
+}
